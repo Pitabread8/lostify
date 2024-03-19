64f19398558093e16a48aa8584a2fd621933c7e6
Sita <sitavemuri8@gmail.com>
2024-03-19 15:16:43 -0700

    new song on click

--- app.vue
+++ app.vue
@@ -1,10 +1,8 @@
 <script setup>
-// import { Buffer } from "buffer";
 const config = useRuntimeConfig();
-// const basic = Buffer.from(`${config.clientID}:${config.clientSecret}`).toString("base64");
-const album = ref();
+const albums = ref();
 
-const { data: token } = await useFetch("https://accounts.spotify.com/api/token", {
+const { data: res } = await useFetch("https://accounts.spotify.com/api/token", {
   method: "POST",
   body: new URLSearchParams({
     grant_type: "client_credentials",
@@ -14,16 +12,16 @@ const { data: token } = await useFetch("https://accounts.spotify.com/api/token",
 });
 
 watch(
-  token,
-  (newToken) => {
-    if (newToken.access_token) {
+  res,
+  (newRes) => {
+    if (newRes.access_token) {
       useFetch("https://api.spotify.com/v1/search?q=tag:hipster&type=album&limit=50&offset=0", {
         method: "GET",
         headers: {
-          Authorization: `Bearer ${newToken.access_token}`,
+          Authorization: `Bearer ${newRes.access_token}`,
         },
       }).then((a) => {
-        album.value = a.data;
+        albums.value = a.data;
       });
     }
   },
@@ -36,15 +34,25 @@ watch(
 
 <script>
 export default {
+  data() {
+    return {
+      num: null,
+    };
+  },
   methods: {
-    embedSrc(a) {
-      const num = Math.floor(Math.random() * a.value["albums"]["items"].length);
-      return `https://open.spotify.com/embed/album/${a.value["albums"]["items"][num]["external_urls"]["spotify"].substring(31)}`;
+    getAlbums(albums) {
+      this.num = Math.floor(Math.random() * albums.value["albums"]["items"].length);
+    },
+    embedSrc(albums) {
+      if (this.num != null) {
+        return `https://open.spotify.com/embed/album/${albums.value["albums"]["items"][this.num]["external_urls"]["spotify"].substring(31)}`;
+      }
     },
   },
 };
 </script>
 
 <template>
-  <iframe class="p-4 w-screen" title="Spotify Embed" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" :src="embedSrc(album)"></iframe>
+  <button @click="getAlbums(albums)">test</button>
+  <iframe v-if="albums != null" class="w-screen p-4" title="Spotify Embed" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" :src="embedSrc(albums)"></iframe>
 </template>
