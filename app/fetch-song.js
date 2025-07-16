c58ac5bb137c342d36e7dd3d0997598918be14d7
Sita Vemuri <sitavemuri8@gmail.com>
2025-07-15 23:18:37 -0700

    fetch track from album

--- app/fetch-song.js
+++ app/fetch-song.js
@@ -1,22 +1,25 @@
 "use client"
 
-import { useState } from 'react'
 import { generateData } from './hooks/getData';
+import { useState } from 'react';
 
 export default function FetchSong(props) {
     const { token } = props;
     const [num, setNum] = useState(0);
 
-    const { data, loading, error } = generateData(token, "https://api.spotify.com/v1/search?q=tag:hipster&type=album&limit=50&offset=0");
-    if (loading) return <p>Loading music...</p>;
-    if (error) return <p>Error: {error}</p>;
+    const { data: albumData, loading: albumLoading, error: albumError } = generateData(token, "https://api.spotify.com/v1/search?q=tag:hipster&type=album&limit=50&offset=0");
+
+    const albumId = albumData?.albums?.items?.[num]?.id;
+
+    const { data: trackData, loading: trackLoading, error: trackError } = generateData(token, `https://api.spotify.com/v1/albums/${albumId}/tracks`);
 
     return (
         <>
-            <div>
-                <iframe className="rounded-xl w-96 h-48" title="Spotify Embed" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" src={`https://open.spotify.com/embed/album/${data["albums"]["items"][num]["external_urls"]["spotify"].substring(31)}`}></iframe>
-                <button onClick={() => setNum(Math.floor(Math.random() * data["albums"]["items"].length))} className="text-center p-4 bg-[#1DB954] rounded-lg text-xl">Next Song</button>
-            </div>
+            {/* {albumLoading && <p>Loading albums...</p>} */}
+            {/* {trackLoading && <p>Loading tracks...</p>} */}
+
+            {trackData && <iframe className="rounded-xl w-[50vw] h-72" title="Spotify Embed" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" src={`https://open.spotify.com/embed/track/${trackData["items"][0]["external_urls"]["spotify"].substring(31)}`}></iframe>}
+            <button onClick={() => setNum(Math.floor(Math.random() * albumData["albums"]["items"].length))} className="text-center p-4 bg-[#1DB954] rounded-lg text-xl">Next Song</button>
         </>
     )
 }
