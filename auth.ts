22443657eb2fdbc98838841830937d07c67dba50
Sita Vemuri <sitavemuri8@gmail.com>
2025-07-16 20:17:10 -0700

    fix vercel error

--- auth.ts
+++ auth.ts
@@ -4,11 +4,19 @@ import Spotify from "next-auth/providers/spotify";
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         Spotify({
-            authorization: "https://accounts.spotify.com/authorize?scope=user-read-email,user-read-email,playlist-read-private,playlist-read-collaborative,playlist-modify-private,playlist-modify-public",
+            authorization: {
+                url: "https://accounts.spotify.com/authorize",
+                params: {
+                    scope: "user-read-email user-read-private playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public",
+                },
+            },
             clientId: process.env.AUTH_SPOTIFY_ID || "",
             clientSecret: process.env.AUTH_SPOTIFY_SECRET || "",
         }),
     ],
+    session: {
+        strategy: "jwt",
+    },
     callbacks: {
         async jwt({ token, account }) {
             if (account) {
